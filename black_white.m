bcb37dc6adc410e5189891b25b17e69cf29db788
1049870111@qq.com <5986342ko>
2018-05-01 21:58:29 +0800

    CNN的框架加入
    cnn

--- black_white.m
+++ black_white.m
@@ -2,14 +2,31 @@ img1 = imread('E:\省新苗\项目代码\图像处理\first_ML\图像处理库\black_half.jpg');
 img2 = imread('E:\省新苗\项目代码\图像处理\first_ML\图像处理库\white_half.jpg');
                                   %将两图像数据读取到数组变量中，显然数组为M*N*3;
 figure,imshow(img1);
-figure,imshow(img2);              %开窗，显示调用的图像，变量依然保存在变量数组中
+   px = 1200;
+   py = 1200;
+   step = 200;
+   x1 = px - step;
+   x2 = px + step;
+   y1 = py - step;
+   y2 = py + step;
+   hold on;
+   rectx = [x1 x2 x2 x1 x1];
+   recty = [y1 y1 y2 y2 y1];
+plot(rectx, recty, 'linewidth',4);%框定
 
-img1 = im2bw(img1);%（图像分割）转化为二值图
-img2 =im2bw(img2);
 
-img1 = not(img1);%把图像想表达的内容变成1
-figure,imshow(img1);
-figure,imshow(img2);
+% figure,imshow(img2);              %开窗，显示调用的图像，变量依然保存在变量数组中
+% 
+% img1 = im2bw(img1);%（图像分割）转化为二值图
+% img2 =im2bw(img2);
+% 
+% img1 = not(img1);%把图像想表达的内容变成1
+% figure,imshow(img1);
+% figure,imshow(img2);
+
+% text(0.1,0.1,'第一类');  
+% text(1.1,0.1,'第二类');  
+% text(0.1,1,'第三类');
 
 % [B,L] = bwboundaries(img);
 % figure,imshow(img);
--- test.m
+++ test.m
@@ -1,15 +1,18 @@
 %实现KNN算法  
+
 %%算法描述  
 %1、初始化训练集和类别；  
 %2、计算测试集样本与训练集样本的欧氏距离；  
 %3、根据欧氏距离大小对训练集样本进行升序排序；  
 %4、选取欧式距离最小的前K个训练样本，统计其在各类别中的频率；  
 %5、返回频率最大的类别，即测试集样本属于该类别。  
+
 close all;  
 clc;  
   
 %%算法实现  
 %step1、初始化训练集、测试集、K值  
+
 %创建一个三维矩阵，二维表示同一类下的二维坐标点，第三维表示类别  
   
 trainData1=[0 0;0.1 0.3;0.2 0.1;0.2 0.2];%第一类训练数据  
@@ -56,7 +59,7 @@ distance_sort=sort(distance);
 num1=0;%第一类出现的次数  
 num2=0;%第二类出现的次数  
 num3=0;%第三类出现的次数  
-sum=0;%sum1,sum2,sum3的和  
+sum=0;%sum1,sum2,sum3的和 
 for i=1:K  
     for j=1:4  
         if distance1(j)==distance_sort(i)  
@@ -80,7 +83,7 @@ class=[num1 num2 num3];
 classname=find(class(1,:)==max(class));  
   
 fprintf('测试点（%f %f）属于第%d类',testData(1),testData(2),classname);  
-  
+  fprintf('\n');
 %%使用绘图将训练集点和测试集点绘画出来  
 figure(1);  
 hold on;  
@@ -92,4 +95,4 @@ end
 plot(testData(1),testData(2),'x');  
 text(0.1,0.1,'第一类');  
 text(1.1,0.1,'第二类');  
-text(0.1,1,'第三类');  
+text(0.1,1,'第三类');
